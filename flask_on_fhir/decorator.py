4c83fceeb34f6cb3ed0ea745995a25f3c279a7a2
jiaola <djiao@jhu.edu>
2020-08-04 23:49:42 -0400

    Add read decorator

--- flask_on_fhir/decorator.py
+++ flask_on_fhir/decorator.py
@@ -1,8 +1,27 @@
 from functools import update_wrapper
 from flask import make_response, request, current_app
 from flask_on_fhir.core import *
+from functools import wraps
+from fhirclient.models.bundle import Bundle
+
 
 LOG = logging.getLogger(__name__)
+
+
+class read(object):
+    """ a decorator for the Read operation"""
+    def __init__(self, fields, envelope=None):
+        self.fields = fields
+        self.envelope = envelope
+
+    def __call__(self, _func):
+        @wraps(_func)
+        def wrapper(*args, **kwargs):
+            resp = _func(*args, **kwargs)
+            if isinstance(resp, Bundle):
+                # do something about bundle?
+                ...
+            return resp.as_json(), 200
 
 
 def fhir_resource(*args, **kwargs):
