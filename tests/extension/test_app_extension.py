c83160dcf159079c4691b9779176bf70d183e43e
jiaola <djiao@jhu.edu>
2020-08-15 20:49:40 -0400

    Add test for decorating method and use add_fhir_resource_read

--- tests/extension/test_app_extension.py
+++ tests/extension/test_app_extension.py
@@ -4,7 +4,7 @@ def test_metadata(client, fhir):
     assert res.json['resourceType'] == 'CapabilityStatement'
 
 
-def test_add_rest_operation(client, fhir, code_system):
+def test_add_rest_operation_in_class(client, fhir, code_system):
     class ResourceTest:
         @classmethod
         def resource_type(cls):
@@ -15,4 +15,18 @@ def test_add_rest_operation(client, fhir, code_system):
             return code_system
 
     assert 'CodeSystem' in fhir.fhir_resources
+
+
+def test_add_rest_operation_in_function(client, fhir, code_system):
+    @fhir.read(params={})
+    def read_op(resource_id: str):
+        return code_system
+
+    fhir.add_fhir_resource_read('CodeSystem', read_op)
+    assert 'CodeSystem' in fhir.fhir_resources
+    assert 'read' in fhir.fhir_resources['CodeSystem'].rest_operations
+
+    res = client.get('/CodeSystem/summary')
+    assert res.status_code == 200
+
 
